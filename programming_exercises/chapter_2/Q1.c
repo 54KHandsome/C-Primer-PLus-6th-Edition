2fe020ece6a24598d41e7e66117f35961cd0b60a
Zhihao Pan <pan82396610@tamu.edu>
2020-06-24 13:09:30 -0500

    Finished Q1, Q3, Q4, Q5, and Q8 in Chapter 2

---
+++ programming_exercises/chapter_2/Q1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void){
+    printf("Zhihao Pan\n");
+    printf("Zhihao \nPan\n");
+    printf("Zhihao Pan");
+    return 0;
+}
---
+++ programming_exercises/chapter_2/Q3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(void){
+    int age,year,month;
+    age = 23;
+    year = age;
+    month = year*12;
+    printf("year = %d, month = %d", year, month);
+    return 0;
+}
